3a87fc937dcdaf2b848fd7c20181602f6168e033
root <root@localhost.localdomain>
2017-07-03 18:15:28 +0530

    login_update

--- login/src/pages/login/login.ts
+++ login/src/pages/login/login.ts
@@ -12,38 +12,32 @@ import { AuthserviceProvider } from "../../providers/authservice/authservice";
 
 export class LoginPage {
   result: any;
- body :any;
+  body :any;
   resultlogin: any;
   loading: any;
 
   
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public auth:AuthserviceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public auth:AuthserviceProvider)
+ {
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
-onlogin(){
+onlogin()
+    {
 
- let login_email="tavvaforu@gmail.com";
- let login_password="123456"
-   this.body={
-     login_email:login_email,
-     login_password:login_password
-   }; 
-    let body1 = new FormData();
-    body1.append('login_email', login_email);
-    body1.append('login_password', login_password);
-
-   console.log(this.body); 
-   console.log(body1); 
-    //let body1 = 'login_email=' + login_email + '&login_password=' + login_password;
-    this.auth.login(this.body).subscribe(data => {
-    this.result = data
-console.log(data); 
-});
-   
-   
-}
+        let login_email="tavvaforu@gmail.com";
+        let login_password="123456"
+          this.body={
+                login_email:login_email,
+                login_password:login_password
+          }; 
+          
+            console.log(this.body); 
+            this.auth.login(this.body).subscribe(data => {this.result = data
+            console.log(data); 
+        });
+          
+      
+    }
 }
